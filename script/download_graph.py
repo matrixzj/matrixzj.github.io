ef31cbc24ba501b61d09d59746a6a751f453ddfe
matrixzj <matrix.zj@qq.com>
2021-06-14 09:57:57 +0000

    There are 2 files have been modified at 2021-06-14 09:57:56 . 	-rwxr-xr-x. 1 jzou jzou 1692 Jun 14 09:57 script/download_graph.py 	-rw-r--r--. 1 jzou jzou 1655 Jun 14 09:57 script/download_graph.pyc No files have been added. No files have been deleted. No file type has been changed.

--- script/download_graph.py
+++ script/download_graph.py
@@ -5,18 +5,25 @@ import requests
 import magic
 import os
 import sys
+import re
 
 TEMP_PIC_FILE = '/tmp/KEYCAP_PIC_FILE'
 
 def download_graph(url, path, sub_path, name):
-    # proxies = {'http': 'http://10.0.1.77:443'}
 
+    pattern = r"%s\..*" % name
+    for file_name in os.listdir(os.path.join(path, sub_path)):
+        if re.match(pattern, file_name):
+            file_path_name = os.path.join(path, sub_path, file_name)
+            return file_path_name
+
+    # proxies = {'http': 'http://10.0.1.77:443'}
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     response = requests.get(url, headers=headers)
     output_file = open(TEMP_PIC_FILE,'wb')
     output_file.write(response.content)
     output_file.close()
-
+ 
     file_format = magic.from_file(TEMP_PIC_FILE, mime=True)
         
     if file_format == 'image/jpeg':
@@ -36,4 +43,8 @@ def download_graph(url, path, sub_path, name):
 if __name__ == '__main__':
     print(sys.argv[1])
     pic_url = sys.argv[1]
-    download_graph(pic_url, '/tmp', '', 'imagetemp')
+#    download_graph(pic_url, '/tmp', '', 'imagetemp')
+    KEYCAP_ASSETS_WITH_PATH = 'assets/images/gmk-keycaps/Sixes'
+    sub_path='kits_pics'
+    name = 'base'
+    download_graph(pic_url, KEYCAP_ASSETS_WITH_PATH, sub_path, name)
